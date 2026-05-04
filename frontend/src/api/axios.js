4567a7b75ea89a44f1ac4f8f62241cdad73acb5d
babader08 <babamboup697@gmail.com>
2026-05-04 18:19:11 +0000

    Ddd

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,7 +3,7 @@ import authService from "../services/authService";
 import toast from "react-hot-toast";
 
 const api = axios.create({
-  baseURL: "https://babader-app-gestion-stock.hf.space/api",
+  baseURL: "/api",
   withCredentials: true,
   timeout: 5000,
 });
